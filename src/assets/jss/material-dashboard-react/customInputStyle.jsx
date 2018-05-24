8cb7a448dac45bd6f6ede5d37aa60dca4ae4eedc
ue <ugurerdal93@gmail.com>
2018-05-25 00:19:13 +0300

    fixed couple bugs also overwrite to material ui

--- src/assets/jss/material-dashboard-react/customInputStyle.jsx
+++ src/assets/jss/material-dashboard-react/customInputStyle.jsx
@@ -2,70 +2,65 @@
 // // // CustomInput styles
 // #############################
 
-import {
-  primaryColor,
-  dangerColor,
-  successColor,
-  defaultFont
-} from "../material-dashboard-react";
+import { primaryColor, dangerColor, successColor, defaultFont } from '../material-dashboard-react';
 
 const customInputStyle = {
   disabled: {
-    "&:before": {
-      backgroundColor: "transparent !important"
-    }
+    '&:before': {
+      backgroundColor: 'transparent !important',
+    },
   },
   underline: {
-    "&:hover:not($disabled):before,&:before": {
-      backgroundColor: "#D2D2D2",
-      height: "1px !important"
+    '&:hover:not($disabled):before,&:before': {
+      backgroundColor: '#D2D2D2',
+      height: '1px !important',
     },
-    "&:after": {
-      backgroundColor: primaryColor
-    }
+    '&:after': {
+      backgroundColor: primaryColor,
+    },
   },
   underlineError: {
-    "&:after": {
-      backgroundColor: dangerColor
-    }
+    '&:after': {
+      backgroundColor: dangerColor,
+    },
   },
   underlineSuccess: {
-    "&:after": {
-      backgroundColor: successColor
-    }
+    '&:after': {
+      backgroundColor: successColor,
+    },
   },
   labelRoot: {
     ...defaultFont,
-    color: "#AAAAAA",
-    fontWeight: "400",
-    fontSize: "14px",
-    lineHeight: "1.42857"
+    color: '#AAAAAA',
+    fontWeight: '400',
+    fontSize: '14px',
+    lineHeight: '1.42857',
   },
   labelRootError: {
-    color: dangerColor
+    color: dangerColor,
   },
   labelRootSuccess: {
-    color: successColor
+    color: successColor,
   },
   feedback: {
-    position: "absolute",
-    top: "18px",
-    right: "0",
-    zIndex: "2",
-    display: "block",
-    width: "24px",
-    height: "24px",
-    textAlign: "center",
-    pointerEvents: "none"
+    position: 'absolute',
+    top: '18px',
+    right: '0',
+    zIndex: '2',
+    display: 'block',
+    width: '24px',
+    height: '24px',
+    textAlign: 'center',
+    pointerEvents: 'none',
   },
   marginTop: {
-    marginTop: "16px"
+    marginTop: '16px',
   },
   formControl: {
-    paddingBottom: "10px",
-    margin: "27px 0 0 0",
-    position: "relative"
-  }
+    paddingBottom: '10px',
+    margin: '27px 0 0 0',
+    position: 'relative',
+  },
 };
 
 export default customInputStyle;
--- src/components/customInput/customInput.jsx
+++ src/components/customInput/customInput.jsx
@@ -1,10 +1,9 @@
-import React from "react";
-import { withStyles, FormControl, InputLabel, Input } from "@material-ui/core";
-import { Clear, Check } from "@material-ui/icons";
-import PropTypes from "prop-types";
-import cx from "classnames";
+import React from 'react';
+import { withStyles, FormControl, InputLabel, Input } from '@material-ui/core';
+import { Clear, Check } from '@material-ui/icons';
+import cx from 'classnames';
 
-import customInputStyle from "../../assets/jss/material-dashboard-react/customInputStyle";
+import customInputStyle from '../../assets/jss/material-dashboard-react/customInputStyle';
 
 function CustomInput({ ...props }) {
   const {
@@ -17,37 +16,34 @@ function CustomInput({ ...props }) {
     error,
     success,
     type,
-    helperText
+    helperText,
   } = props;
 
   const labelClasses = cx({
-    [" " + classes.labelRootError]: error,
-    [" " + classes.labelRootSuccess]: success && !error
+    [` ${classes.labelRootError}`]: error,
+    [` ${classes.labelRootSuccess}`]: success && !error,
   });
   const underlineClasses = cx({
     [classes.underlineError]: error,
     [classes.underlineSuccess]: success && !error,
-    [classes.underline]: true
+    [classes.underline]: true,
   });
   const marginTop = cx({
-    [classes.marginTop]: labelText === undefined
+    [classes.marginTop]: labelText === undefined,
   });
   return (
     <FormControl
       {...formControlProps}
-      className={formControlProps.className + " " + classes.formControl}
+      className={`${formControlProps.className} ${classes.formControl}`}
     >
       {labelText !== undefined ? (
-        <InputLabel
-          className={classes.labelRoot + labelClasses}
-          htmlFor={id}
-          {...labelProps}
-        >
+        <InputLabel className={classes.labelRoot + labelClasses} htmlFor={id} {...labelProps}>
           {labelText}
         </InputLabel>
       ) : null}
       <Input
         classes={{
+          // border-bottom: 0px solid rgba(0, 0, 0, 0);
           root: marginTop,
           disabled: classes.disabled,
           underline: underlineClasses,
@@ -58,23 +54,12 @@ function CustomInput({ ...props }) {
         {...inputProps}
       />
       {error ? (
-        <Clear className={classes.feedback + " " + classes.labelRootError} />
+        <Clear className={`${classes.feedback} ${classes.labelRootError}`} />
       ) : success ? (
-        <Check className={classes.feedback + " " + classes.labelRootSuccess} />
+        <Check className={`${classes.feedback} ${classes.labelRootSuccess}`} />
       ) : null}
     </FormControl>
   );
 }
-
-CustomInput.propTypes = {
-  classes: PropTypes.object.isRequired,
-  labelText: PropTypes.node,
-  labelProps: PropTypes.object,
-  id: PropTypes.string,
-  inputProps: PropTypes.object,
-  formControlProps: PropTypes.object,
-  error: PropTypes.bool,
-  success: PropTypes.bool
-};
 
 export default withStyles(customInputStyle)(CustomInput);
